ca3f6c5447e9af12c7662b43411ed3cc00e3131b
tejendrasingh104 <tejendrasingh104@gmail.com>
2023-01-23 17:58:24 +0530

    improving dark mode and refactoring App video11

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
 
 function App(){
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if(mode === 'dark'){
+      setMode('light')
+      document.body.style.backgroundColor='white'
+    }
+    else{
+      setMode('dark')
+      document.body.style.backgroundColor='#042970'
+    }
+  }
   return(
     <>
-    <Navbar title='TextUtils' aboutText='About textUtils'/>
+    <Navbar title='TextUtils' aboutText='About textUtils' mode={mode} toggleMode={toggleMode}/>
     {/* <Navbar /> */}
     {/* <Navbar title='TextUtils'/> */}
     <div className="container my-3">
-      <TextForm heading='Enter the text to analyse'/>
+      <TextForm heading='Enter the text to analyse' mode={mode}/>
     </div>
-    <div className='container'>
+    {/* <div className='container'>
       <About/>
-    </div>
+    </div> */}
     
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -28,22 +28,34 @@ export default function TextForm(props) {
 
   return (
     <>
-        <div className="container">
+        <div className="container" style={{color:props.mode === 'dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} placeholder={'enter your text'} onChange={handleOnChange} id='mybox' rows='8'></textarea>
+            <textarea 
+                className="form-control" 
+                value={text} 
+                placeholder={'enter your text'} 
+                onChange={handleOnChange} 
+                id='mybox' 
+                rows='8' 
+                style={{
+                    backgroundColor:props.mode === 'light'?'white':'grey', 
+                    color:props.mode === 'dark'?'white':'black'
+                }}
+            >
+            </textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>UpperCase</button>
             <button className="btn btn-primary mx-1" onClick={handleLowClick}>LowerCase</button>
             <button className="btn btn-primary mx-1" onClick={handleClear}>Clear</button>
             <button className="btn btn-primary mx-1" onClick={handleRemoveExtraSpace}>Remove Extra Space</button>
         </div>
-        <div className="container my-2">
+        <div className="container my-2" style={{color:props.mode === 'dark'?'white':'black'}}>
             <h2>Your Text Summary</h2>
             <p>{text.split(' ').filter(x => x!== '').length} words and {text.length} character</p>
             <p>{0.008*text.split(' ').filter(x => x!== '').length} Minutes to Read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:'Enter something in text area to preview it here'}</p>
         </div>
     </>
   
